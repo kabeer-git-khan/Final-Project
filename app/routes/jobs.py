7fdf12b3b9ff093e2c2cba33fc52052eb6991040
Kabeer Khan <kdotkhan22@gmail.com>
2026-01-08 19:36:27 +0500

    Job Added

--- app/routes/jobs.py
+++ app/routes/jobs.py
@@ -1,3 +1,32 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from models.job import JobCreate
+from store.memory import jobs_seed
 
 router = APIRouter()
+
+
+@router.post("/jobs")
+def add_job(job: JobCreate):
+    
+    jobs_seed{
+        "id":job.id,
+        "name":job.name,
+        "description":job.description,
+        "industry": job.industry,
+        "keyword":job.keywords
+    }
+    return job["id"]
+
+
+@router.get("/jobs")
+def get_all_jobs():
+    return jobs_seed
+
+@router.get("/job/{id}")
+def get_id(id: int):
+    if id not in jobs_seed.get(id):
+        HTTPExceptipn(status_code=404, detail="Not Found")
+        
+    return jobs_seed.get(id)
+
+@router.post()
